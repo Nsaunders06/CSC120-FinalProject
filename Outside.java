63bc3adbeddce25a8078dca1b7d1a13ea663e22c
Nsaunders06 <nsaunders@smith.edu>
2024-11-21 10:51:41 -0500

    Add files via upload

---
+++ Outside.java
@@ -0,0 +1,16 @@
+public class Outside{
+//Attributes 
+boolean goldenTicket; 
+boolean doorOpen; 
+
+
+/** 
+ * Constructor for Outside 
+ */
+
+public Outside(){
+    this.goldenTicket = false; 
+    this.doorOpen = false; 
+}
+
+}
---
+++ Person.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class Person {
+//attributes 
+int locationX; 
+int locationY; 
+//Arraylist <items> inventory; 
+String roomLocation; 
+String input; 
+
+/**
+ * Constructor 
+ */
+public Person(String roomLocation, String input){
+    this.locationX = -2; 
+    this.locationY = -2; 
+    this.roomLocation = roomLocation; 
+    this.input = input; 
+}
+
+public boolean open(int locationX, int locationY, String room){
+    return false; 
+} // Come back and fix this once more stuff is in place 
+
+public Item grab(int locationX, int locationY, String room){ 
+    // This will fist check if there is an item there and then will add the item to the inventory array list 
+}
+
+public void viewInventory (Items inventory){ 
+    System.out.println(inventory);
+}
+
+public void viewLocation (int locationX, int locationY){
+    System.out.println("You are at" + locationX + "," + locationY);
+}
+
+}
