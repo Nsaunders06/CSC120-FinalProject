94b7a6c4a6aa4a0f08ff914bca972381d9e15395
Nina Saunders <nsaunders@smith.edu>
2024-12-03 11:58:41 -0500

    GameLoop Updated

--- GameLoop.java
+++ GameLoop.java
@@ -6,5 +6,16 @@ public class GameLoop {
         Scanner userInput = new Scanner(System.in); 
         String userResponse = ""; 
 
+        //Welcome message 
+        System.out.println("******************");
+        System.out.println("WELCOME TO MY GAME");
+        System.out.println("******************");
+
+        do{ 
+         System.out.println("You are still stuck. Keep going :)");
+
+        } while (stillPlaying);
+
+
     }
 }
