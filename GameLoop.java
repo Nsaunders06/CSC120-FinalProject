42ea9692046c293b5bdc6cbda854eeae65091aaf
Nina Saunders <nsaunders@smith.edu>
2024-12-03 11:29:51 -0500

    Game loop

---
+++ GameLoop.java
@@ -0,0 +1,7 @@
+import java.util.Scanner; 
+public class GameLoop {
+    public static void main(String[] args) {
+        // let us know when the loop should end 
+        boolean stillPlaying = true; 
+    }
+}
